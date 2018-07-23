91fcce5c7a78f5b5fadf0f8f2301d3155cb7ba18
Astellon <synchronize2525@gmail.com>
2018-07-23 20:13:13 +0900

    new test

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 ## テスト
 
 ```
-$ g++ -std=c++11 -O3 phase.cpp test.cpp
+$ g++ -std=c++11 -O3 sin.cpp phase.cpp test.cpp
 $ ./a.out
 ```
 
@@ -14,22 +14,17 @@ $ ./a.out
 
 参考までに手元での結果を示す。
 ```
-2560000 times call
-
 error (root mean square)
- error of table (uint32) : 2.43049e-06
- error of table (double) : 2.43027e-06
- error of pade           : 2.38454e-05
+ error of table (uint32) : 0.707105
+ error of table (double) : 0.707105
+ error of pade           : 0.706958
 
-time (using double phase)
- std::sin time   : 65191100
- table::sin time : 34935000 (1.86607x faster)
- pade::sin time  : 29021700 (2.24629x faster)
-
-time (using uint32 phase)
- table::sin time : 19628300 (3.32128x faster)
- pade::sin time  : 27823800 (2.343x faster)
-
+Phase + std::sin : 79551700
+double + std::sin : 68806800
+Phase + pade::sin : 25896600
+double + pade::sin : 26198400
+Phase + table::sin : 21641900
+double + table::sin : 38154200
 ```
 
 ## 覚書
--- test.cpp
+++ test.cpp
@@ -3,8 +3,12 @@
 #include <iostream>
 
 #include "sin.h"
+#include "timer.h"
 
 int main() {
+  using MS = std::chrono::milliseconds;
+  using NS = std::chrono::nanoseconds;
+
   const double freq = 440.0;
   const double sample_rate = 44100.0;
   const int buffer_size = 256;
@@ -23,83 +27,19 @@ int main() {
     for (int i = 0; i < buffer_size; i++) {
       sample = std::sin(phase.toDouble());
 
-      double dif_pade = sample - approx::pade::sin(phase);
-      double dif_table = sample - approx::table::sin(phase);
-      double dif_table_inter = sample - approx::table::sin(phase.toDouble());
+      double dif_pade = sample - approx::pade::sin(angle);
+      double dif_table = sample - approx::table::sin(angle);
+      double dif_table_inter = sample - approx::table::sin(angle);
       // mean squared error
       err_pade += dif_pade * dif_pade;
       err_table += dif_table * dif_table;
       err_table_d += dif_table_inter * dif_table_inter;
 
-      phase += phase_delta;
+      angle += angle_delta;
+      if (angle > 2*approx::PI) angle -= 2*approx::PI;
       sample = 0;
     }
   }
-
-  auto start1 = std::chrono::system_clock::now();
-  for (int rep = 0; rep < repeat; rep++) {
-    for (int i = 0; i < buffer_size; i++) {
-      sample = std::sin(phase.toDouble());
-      phase += phase_delta;
-    }
-  }
-  auto end1 = std::chrono::system_clock::now();
-  auto dur1 = end1 - start1;
-
-
-  auto start2 = std::chrono::system_clock::now();
-  for (int rep = 0; rep < repeat; rep++) {
-    for (int i = 0; i < buffer_size; i++) {
-      sample = approx::table::sin(angle);
-      angle += angle_delta;
-      if (angle > 2*approx::PI) angle -= 2*approx::PI;
-    }
-  }
-  auto end2 = std::chrono::system_clock::now();
-  auto dur2 = end2 - start2;
-
-  auto start3 = std::chrono::system_clock::now();
-  for (int rep = 0; rep < repeat; rep++) {
-    for (int i = 0; i < buffer_size; i++) {
-      sample = approx::pade::sin(angle);
-      angle += angle_delta;
-      if (angle > 2*approx::PI) angle -= 2*approx::PI;
-    }
-  }
-  auto end3 = std::chrono::system_clock::now();
-  auto dur3 = end3 - start3;
-
-  auto start4 = std::chrono::system_clock::now();
-  for (int rep = 0; rep < repeat; rep++) {
-    for (int i = 0; i < buffer_size; i++) {
-      sample = approx::table::sin(phase);
-      phase += phase_delta;
-    }
-  }
-  auto end4 = std::chrono::system_clock::now();
-  auto dur4 = end4 - start4;
-
-  auto start5 = std::chrono::system_clock::now();
-  for (int rep = 0; rep < repeat; rep++) {
-    for (int i = 0; i < buffer_size; i++) {
-      sample = approx::pade::sin(phase);
-      phase += phase_delta;
-    }
-  }
-  auto end5 = std::chrono::system_clock::now();
-  auto dur5 = end5 - start5;
-
-  int dur1_i = std::chrono::duration_cast<std::chrono::nanoseconds>(dur1).count();
-  int dur2_i = std::chrono::duration_cast<std::chrono::nanoseconds>(dur2).count();
-  int dur3_i = std::chrono::duration_cast<std::chrono::nanoseconds>(dur3).count();
-  int dur4_i = std::chrono::duration_cast<std::chrono::nanoseconds>(dur4).count();
-  int dur5_i = std::chrono::duration_cast<std::chrono::nanoseconds>(dur5).count();
-
-  double faster_table = static_cast<double>(dur1_i)/dur2_i;
-  double faster_pade = static_cast<double>(dur1_i)/dur3_i;
-
-  std::cout << buffer_size*repeat << " times call" << std::endl;
-  std::cout << std::endl;
 
   std::cout << "error (root mean square)" << std::endl;
   std::cout << " error of table (uint32) : " << sqrt(err_table/repeat/buffer_size) << std::endl;
@@ -107,18 +47,81 @@ int main() {
   std::cout << " error of pade           : " << sqrt(err_pade/repeat/buffer_size) << std::endl;
   std::cout << std::endl;
 
-  std::cout << "time (using double phase)" << std::endl;
-  std::cout << " std::sin time   : " << dur1_i << std::endl;
-  std::cout << " table::sin time : " << dur2_i << " (" << faster_table << "x faster)" <<  std::endl;
-  std::cout << " pade::sin time  : " << dur3_i << " (" << faster_pade << "x faster)" << std::endl;
-  std::cout << std::endl;
+  // Phase + std::sin
+  int time = 0;
+  {
+    Timer<NS> T(&time);
+    for (int rep = 0; rep < repeat; rep++) {
+      for (int i = 0; i < buffer_size; i++) {
+        sample = std::sin(phase.toDouble());
+        phase += phase_delta;
+      }
+    }
+  }
+  std::cout << "Phase + std::sin : " << time << std::endl;
 
-  faster_table = static_cast<double>(dur1_i)/dur4_i;
-  faster_pade = static_cast<double>(dur1_i)/dur5_i;
+  // double + std::sin
+  {
+    Timer<NS> T(&time);
+    for (int rep = 0; rep < repeat; rep++) {
+      for (int i = 0; i < buffer_size; i++) {
+        sample = std::sin(angle);
+        angle += angle_delta;
+        if (angle > 2*approx::PI) angle -= 2*approx::PI;
+      }
+    }
+  }
+  std::cout << "double + std::sin : " << time << std::endl;
 
-  std::cout << "time (using uint32 phase)" << std::endl;
-  std::cout << " table::sin time : " << dur4_i << " (" << faster_table << "x faster)" <<  std::endl;
-  std::cout << " pade::sin time  : " << dur5_i << " (" << faster_pade << "x faster)" << std::endl;
-  std::cout << std::endl;
+  // Phase + pade::sin
+  {
+    Timer<NS> T(&time);
+    for (int rep = 0; rep < repeat; rep++) {
+      for (int i = 0; i < buffer_size; i++) {
+        sample = approx::pade::sin(phase);
+        phase += phase_delta;
+      }
+    }
+  }
+  std::cout << "Phase + pade::sin : " << time << std::endl;
+
+  // double + pade::sin
+  {
+    Timer<NS> T(&time);
+    for (int rep = 0; rep < repeat; rep++) {
+      for (int i = 0; i < buffer_size; i++) {
+        sample = approx::pade::sin(angle);
+        angle += angle_delta;
+        if (angle > 2*approx::PI) angle -= 2*approx::PI;
+      }
+    }
+  }
+  std::cout << "double + pade::sin : " << time << std::endl;
+
+  // Phase + table::sin
+  {
+    Timer<NS> T(&time);
+    for (int rep = 0; rep < repeat; rep++) {
+      for (int i = 0; i < buffer_size; i++) {
+        sample = approx::table::sin(phase);
+        phase += phase_delta;
+      }
+    }
+  }
+  std::cout << "Phase + table::sin : " << time << std::endl;
+
+  // double + table::sin
+  {
+    Timer<NS> T(&time);
+    for (int rep = 0; rep < repeat; rep++) {
+      for (int i = 0; i < buffer_size; i++) {
+        sample = approx::table::sin(angle);
+        angle += angle_delta;
+        if (angle > 2*approx::PI) angle -= 2*approx::PI;
+      }
+    }
+  }
+  std::cout << "double + table::sin : " << time << std::endl;
+
   return 0;
 }
